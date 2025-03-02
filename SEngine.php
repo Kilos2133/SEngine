7850aaf257f85e3229f147bd94e37ecf2845afac
Kilos <kito21332s@gmail.com>
2025-03-02 23:36:37 +0200

    My first commit

---
+++ SEngine.php
@@ -0,0 +1,23 @@
+<?php
+// !!! TODO 1: ваш код обробки GET запиту; виконання запиту через cURL у пошукову систему; підготовка даних для малювання
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h2>My Browser</h2>
+<form method="GET" action="/index.php">
+    <label for="search">Search:</label>
+    <input type="text" id="search" name="search" value=""><br><br>
+    <input type="submit" value="Submit">
+</ form >
+
+<?php
+// !!! TODO 2: код відображення відповіді
+?>
+</body>
+</html>
